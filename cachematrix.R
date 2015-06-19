81a32529c4334d31412c9c3707cd191d2b75c74e
Seb Griffiths <sebg@Sebs-MacBook-Air.local>
2015-06-19 23:45:38 +1000

    Initial commit. Inverse matrix function tested and working as per my understanding. Added some comments explaining the role of each function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## this function takes a numeric matrix as input and  
+## returns a special vector for use in cacheSolve function
+## the special vector is basically a list of solve functions
+## stored into variables that part of the parent environment
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## this function takes makeCacheMatrix as input and
+## first checks the parent environment for an existing
+## cached result, if found return the cached value, 
+## else computer the inverse (solve) function and then
+## store the computed value into the parent environment
+## for future calls 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
